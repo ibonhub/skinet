592431d48dd2b72fe7060edf7c836a4333ce0b00
IB <ibonline@outlook.com>
2021-11-10 00:43:16 +0100

    Intermediate (not running)

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,11 +1,8 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
-using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
