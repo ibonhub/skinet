8fcfbcd444318c3e30eb47f9bf19a3362d2eab44
IB <ibonline@outlook.com>
2021-11-12 17:12:10 +0100

    28. Applying the migrations and creating the Database at app startup

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,8 +1,9 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
-using Core.Interfaces;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -10,24 +11,23 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductRepository _repo;
-
-        public ProductsController(IProductRepository repo)
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
         {
-            this._repo = repo;
+            _context = context;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _repo.GetProductsAsync();
+            var products = await _context.Products.ToListAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _repo.GetProductByIdAsync(id);
+            return await _context.Products.FindAsync(id);
         }
     }
 }
--- Core/Entities/ProductBrand.cs
+++ Core/Entities/ProductBrand.cs
@@ -2,7 +2,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Core.Entities
 {
-    [Keyless]
     public class ProductBrand : BaseEntity
     {
         public string Name { get; set; }
