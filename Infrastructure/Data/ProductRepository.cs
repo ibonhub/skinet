1a62b6e6aca072733b52e625beef38e3f4f544a3
IB <ibonline@outlook.com>
2021-11-17 10:20:53 +0100

    31. Eager loading of navigation properties

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -17,9 +17,21 @@ namespace Infrastructure.Data
 
         public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync() => await _context.ProductBrands.ToListAsync();
 
-        public async Task<Product> GetProductByIdAsync(int id) => await _context.Products.FindAsync(id);
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _context.Products
+                .Include(p => p.ProductBrand)
+                .Include(p => p.ProductType)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
 
-        public async Task<IReadOnlyList<Product>> GetProductsAsync() => await _context.Products.ToListAsync();
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            return await _context.Products
+                .Include(p => p.ProductBrand)
+                .Include(p => p.ProductType)
+                .ToListAsync();
+        }
 
         public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync() => await _context.ProductTypes.ToListAsync();
     }
