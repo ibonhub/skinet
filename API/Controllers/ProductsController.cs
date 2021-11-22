45cde304770e5f23a755277cae93e1eb39f9f0e3
IB <ibonline@outlook.com>
2021-11-22 21:32:47 +0100

    44. Adding AutoMapper to the API project

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
@@ -18,14 +19,17 @@ namespace API.Controllers
         private readonly IGenericRepository<Product> _productRepo;
         private readonly IGenericRepository<ProductBrand> _productBrandRepo;
         private readonly IGenericRepository<ProductType> _productTypeRepo;
+        private readonly IMapper _mapper;
 
         public ProductsController(IGenericRepository<Product> productRepo
             , IGenericRepository<ProductBrand> productBrandRepo
-            , IGenericRepository<ProductType> productTypeRepo)
+            , IGenericRepository<ProductType> productTypeRepo
+            , IMapper mapper)
         {
            _productRepo = productRepo;
            _productBrandRepo = productBrandRepo;
            _productTypeRepo = productTypeRepo;
+           _mapper = mapper;
         }
 
         [HttpGet]
@@ -54,16 +58,7 @@ namespace API.Controllers
 
             var product = await _productRepo.GetEntityWithSpec(spec);
 
-            return new ProductToReturnDto
-            {
-                Id = product.Id,
-                Name = product.Name,
-                Description = product.Description,
-                Price = product.Price,
-                PictureUrl = product.PictureUrl,
-                ProductType = product.ProductType.Name,
-                ProductBrand = product.ProductBrand.Name
-            };
+            return _mapper.Map<Product, ProductToReturnDto>(product);
         }
 
         [HttpGet("brands")]
