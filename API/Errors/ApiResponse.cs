910b6728df7889b68c55b6193b5f7fe2da178bf9
IB <ibonline@outlook.com>
2021-11-24 22:46:15 +0100

    51. Creating a consistent error response from the API

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "bad request",
+                401 => "authorization",
+                404 => "not found",
+                500 => "server error",
+                _ => null
+            };
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+    }
+}
