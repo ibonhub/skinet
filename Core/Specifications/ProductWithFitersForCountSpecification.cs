dc2ac9e3e30d6f245e1e54ac159ac7a2677610e0
ib <ib@example.com>
2021-12-05 12:58:24 +0100

    67. Adding the search functionality

--- Core/Specifications/ProductWithFitersForCountSpecification.cs
+++ Core/Specifications/ProductWithFitersForCountSpecification.cs
@@ -6,9 +6,12 @@ namespace Core.Specifications
     {
         public ProductWithFitersForCountSpecification(ProductSpecParams productParams)
             :base(x =>
-                  (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) 
-                    && (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
-                 )
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search))
+                    &&
+                (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) 
+                    && 
+                (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+            )
         {
             
         }
