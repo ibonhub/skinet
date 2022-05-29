6e0b5d2033f764b36b61064a2c9177bcd2479d7f
IB <ibonline@outlook.com>
2022-05-29 15:23:56 +0200

    100. Adding the pagination functionality part 3

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -72,4 +72,9 @@ export class ShopComponent implements OnInit {
      this.shopParams.sort = sort;
      this.getProducts();
    }
+
+   onPageChanged(event: any) {
+     this.shopParams.pageNumber = event.page;
+     this.getProducts();
+   }
 }
